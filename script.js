55077b8c77832adc1633ed8090d95b8ebbd866f4
dmitri.antipov1 <dmitri.antipov1@gmail.com>
2021-07-15 11:02:18 +0300

    ended

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
-let counter = document.querySelector('#counter-value')
-var now = moment("2021-07-12"); // new Date().getTime();
-var then = moment().add(60, 'seconds'); // new Date(now + 60 * 1000);
+const countdown = () => {
+    const countDate = new Date('July 13, 2021 00:00:00').getTime()
+    const now = new Date().getTime()
+    const gap = now - countDate
 
-// from then until now
-(function timerLoop() {
-    $().text(moment(then).to(now));
-    $("#counter").text(countdown(now).toString());
-    requestAnimationFrame(timerLoop);
-})();
+    const second = 1000;
+    const minute = second * 60
+    const hour = minute * 60
+    const day = hour * 24
+
+    const textDay = Math.floor(gap / day)
+    const textHour = Math.floor((gap % day) / hour)
+    const textMinute = Math.floor((gap % hour) / minute)
+    const textSecond = Math.floor((gap % minute) / second)
+
+    document.querySelector('#day').innerText = textDay
+    document.querySelector('#hour').innerText = textHour
+    document.querySelector('#minute').innerText = textMinute
+    document.querySelector('#second').innerText = textSecond
+}
+
+setInterval(countdown, 1000)
