b9b483bf434d7af64c7cb4aba8f5e0eae97736b2
dmitri.antipov1 <dmitri.antipov1@gmail.com>
2021-07-16 08:47:20 +0300

    ended

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const countdown = () => {
-    const countDate = new Date('July 13, 2021 00:00:00').getTime()
+    const countDate = new Date('July 15, 2021 13:55:00').getTime()
     const now = new Date().getTime()
     const gap = now - countDate
 
