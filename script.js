d6ca247e5d17577e0b91001cd5820b1d0869e76b
dmitri.antipov1 <dmitri.antipov1@gmail.com>
2022-02-04 21:54:59 +0200

    new date 04 02 2022

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const countdown = () => {
-    const countDate = new Date('January 12, 2022 13:55:00').getTime()
+    const countDate = new Date('February 04, 2022 13:55:00').getTime()
     const now = new Date().getTime()
     const gap = now - countDate
 
